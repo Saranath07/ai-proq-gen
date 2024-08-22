16a43a7c1bc144732094cdfd0a7353b94db89bdc
Saranath07 <saranathp34@gmail.com>
2024-08-22 16:45:35 +0530

    ai-proqgen-1 added

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-def sum_of_ints(s:str)->int:
-    return sum(map(int, filter(str.isdigit, s)))
+def sum_integers_in_string(s:str)->int:
+    return sum(map(int, filter(str.isdigit, ''.join(filter(str.isalpha, s))))
         import sys
         exec(sys.stdin.read())
         
